90a96f84ce567a90b5318fb4ea39ff4d832272e2
tobe <tobeg3oogle@gmail.com>
2014-02-19 15:45:09 +0800

    add static page controller and set home page

--- itidol/config/routes.rb
+++ itidol/config/routes.rb
@@ -1,8 +1,12 @@
 Itidol::Application.routes.draw do
+  get "static_page/home"
+
   resources :comments
 
   resources :idols
 
+  root "static_page#home"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
