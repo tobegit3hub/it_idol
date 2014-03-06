7d6436ac8b10ec1ea57339a4c502a9c9cd4b867b
tobe <tobeg3oogle@gmail.com>
2014-03-07 00:51:11 +0800

    display random motto in home page

--- itidol/app/controllers/static_page_controller.rb
+++ itidol/app/controllers/static_page_controller.rb
@@ -1,7 +1,13 @@
 class StaticPageController < ApplicationController
   
   def home
+      
     @idols = Idol.find(:all, :order => "count DESC")
+
+    if @idols.length > 0
+      @random_motto = @idols.sample.motto
+    end
+      
   end
   
 end
